9cd481486858a01b9058cc3dd0f1a44fb7d2cdae
Nicholas Noble <nicholas100200@gmail.com>
2021-09-23 20:37:09 -0500

    Created loadWeb3() for MetaMask connection

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,54 @@
+import { Component } from 'react';
 import logo from '../logo.svg';
+import Web3 from 'web3';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  async componentDidMount() {
+    await this.loadWeb3();
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+
+    else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    
+  }
+  
+  render() {
+    return ( 
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
