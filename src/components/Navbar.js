32e231041897dd276388aaca11dd12b89ae273c2
Nicholas Noble <nicholas100200@gmail.com>
2021-10-12 16:36:35 -0500

    Shortened wallet address string

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,23 @@ import { Component } from 'react';
 import Identicon from 'identicon';
 import logo from '../logo.svg';
 
+
+
+
 class Navbar extends Component {
+  
+  shortAccountString() {
+    let accountString;
+    if(this.props.account) {
+      accountString = this.props.account.substring(0,6) + '...' + this.props.account.substring(38,42);
+    }
+    else {
+      accountString = '0x0';
+    }
+    return accountString;
+  }
+
+  
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark p-0 text-monospace">
@@ -11,7 +27,9 @@ class Navbar extends Component {
           DStorage
         </h3>
         <ul className="navbar-nav px-3">
-          <b className="text-white px-3">{this.props.account}</b>
+          <b className="text-white px-3">
+            {this.shortAccountString()}
+          </b>
         </ul>
       </nav>
     );
