5f6c1f9edd40f8906830642a593c266653bfbb24
Nicholas Noble <nicholas100200@gmail.com>
2021-10-12 16:36:54 -0500

    created loadBlockchainData function

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import DStorage from '../abis/DStorage.json'
 import { Component } from 'react';
 import logo from '../logo.svg';
 import Web3 from 'web3';
@@ -33,13 +34,40 @@ class App extends Component {
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0]});
 
+    //Network ID
+    const networkId = await web3.eth.net.getId();
+    const networkData = DStorage.networks[networkId];
+    if(networkData) {
+      //Assign contract
+      const dstorage = new web3.eth.Contract(DStorage.abi, networkData.address);
+      this.setState({ dstorage });
+      //Get files amount
+      const filesCount = await dstorage.methods.fileCount().call();
+      this.setState({ filesCount });
+      //Load fikes & sort by newest
+      for(var i = filesCount; i >= 1; i--) {
+        const file = await dstorage.methods.files(i).call();
+        this.setState({
+          files: [...this.state.files, file]
+        });
+      }
+      //console.log(this.state.files)
+    } else {
+      window.alert('DStorage contract not deployed to detected network.');
+    }
+
     this.setState({loading: false});
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      loading: true
+      account: '',
+      dstorage: null,
+      files: [],
+      loading: true,
+      type: null,
+      name: null
     };
   }
   
